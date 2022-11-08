b50de3bef8fd11c4dca07e305d38cc7613661d53
Macias Romero Jorge Humberto <a00573134@itesm.mx>
2022-11-08 07:14:08 -0600

    Algunos diccionarios ya puestos

--- api/src/api.py
+++ api/src/api.py
@@ -114,22 +114,12 @@ en la parte de la izquierda, donde creaste la conexión en vscode
 ahi otra opción que dice "create new playgroun",le das click
 borras todo lo que tiene y escribes lo siguiente
 
-use('signaDB');
-
-db.word.insertMany([
-  {'name':'palabra1', 'video':'link al video', 'img': 'link a la imagen', 'categories':'palabrasBasicas', 'level': 1},
-  {'name':'palabra2', 'video':'link al video', 'img': 'link a la imagen', 'categories':'palabrasBasicas', 'level': 1}
-])
+use('signDB')
 
 el use es para decire que base de datos usar, y el insertMany es una
 funcion que recibe un arreglo de jsons, los cuales son los datos de las palabras
 
-
-IMPORTANTE
-con el ejemplo anterior tendriamos que crear otra tabla para las categorias
-sinembargo hay una alternativa que nos ahorra una tabla, y es hacer
-unicamente una tabla de categorias, esta tabla guardaria un arreglo 
-de palabras, tendriamos algo asi....
+------------------------------------EJEMPLO-------------------------------------------
 
 use('signDB')
 
@@ -143,7 +133,78 @@ db.categories.insertMany([
                                         {'name':'gato', 'video':'link/gato.mp4', 'img': 'link/gato.png'}
                                         ]}                           
 ])
+--------------------------------------------------------------------------------------
 
+db.categories.insertMany([
+    {'name':'verboscomunes', 'level':1, 'words':[
+                                        {'name':'trabajar', 'video':'https://drive.google.com/file/d/1hE6KI9x1R8JARjzqGpqwl3q7gSr9epZF/view?usp=share_link', 'img': 'null'},
+                                        {'name':'vender', 'video':'https://drive.google.com/file/d/1d-0rGtR_vCGtMHrvGGcoMGlTRuKKIiOe/view?usp=share_link', 'img': 'null'},
+                                        {'name':'terminar', 'video':'https://drive.google.com/file/d/1Y6bZb5vZQGVxuFST4tHntIpHa0Mubay_/view?usp=share_link', 'img': 'null'},
+                                        {'name':'sentar', 'video':'https://drive.google.com/file/d/1JNA2N4mKh-IRiuuvaEBD2ou9HQ_RqKuN/view?usp=share_link', 'img': 'null'},
+                                        {'name':'ver', 'video':'https://drive.google.com/file/d/16yHdpwYUjhadnViFZ6-1fcSYlZ4cc0e-/view?usp=share_link', 'img': 'null'},
+                                        {'name':'preguntar', 'video':'https://drive.google.com/file/d/1zsv9aENoH7mQZqh6FfWR6nalDMUWyPZe/view?usp=share_link', 'img': 'null'},
+                                        {'name':'vivir', 'video':'https://drive.google.com/file/d/1N1QCts462umG_XaTxBKE_VQJ2uVqWqcw/view?usp=share_link', 'img': 'null'},
+                                        {'name':'olvidar', 'video':'https://drive.google.com/file/d/1J0hw1XUzdDRzqQUyKIX8TPaRKOcpfkFl/view?usp=share_link', 'img': 'null'},
+                                        {'name':'pensar', 'video':'https://drive.google.com/file/d/1J0hw1XUzdDRzqQUyKIX8TPaRKOcpfkFl/view?usp=share_link', 'img': 'null'},
+                                        {'name':'limpiar', 'video':'https://drive.google.com/file/d/1J0hw1XUzdDRzqQUyKIX8TPaRKOcpfkFl/view?usp=share_link', 'img': 'null'},
+                                        {'name':'jugar', 'video':'https://drive.google.com/file/d/16yIVbzbYGUItoM28aIdCB5JIgq1aFJqN/view?usp=share_link', 'img': 'null'},
+                                        {'name':'hacer', 'video':'https://drive.google.com/file/d/1wk202bGUbrmfys9Ruw1fdclxziXRHtXz/view?usp=share_link', 'img': 'null'},
+                                        {'name':'poder', 'video':'https://drive.google.com/file/d/1mPFtW2_7KscqyCunxGH0t0S35z4sbv-O/view?usp=share_link', 'img': 'null'},
+                                        {'name':'necesitar', 'video':'https://drive.google.com/file/d/1khmC3HBe2cDqYv1VROxII2Vsk5aPhB-n/view?usp=share_link', 'img': 'null'},
+                                        {'name':'platicar', 'video':'https://drive.google.com/file/d/1aSyg8tRERW-NOAnwxS1xHJFCnVlpXogm/view?usp=share_link', 'img': 'null'},
+                                        {'name':'perder', 'video':'https://drive.google.com/file/d/1V5meY2_ZeBrRCrMmLH953L7GYzKBFlaB/view?usp=share_link', 'img': 'null'},
+                                        {'name':'pagar', 'video':'https://drive.google.com/file/d/1LbjI90O4N7l9uEVNvFJgwyUqB6VkzScq/view?usp=share_link', 'img': 'null'},
+                                        {'name':'dormir', 'video':'https://drive.google.com/file/d/1mOXmIHVR7ggYg8CiQQ-M4daSb6F2qX0x/view?usp=share_link', 'img': 'null'},
+                                        {'name':'esperar', 'video':'https://drive.google.com/file/d/1fWe5eihhYHD-HnrhTgSXTvhN0rm9Q_q3/view?usp=share_link', 'img': 'null'},
+                                        {'name':'hablar', 'video':'https://drive.google.com/file/d/15DMpFHkD1wdTg_lP2U29ZtKHr3HLpx7Y/view?usp=share_link', 'img': 'null'},
+                                        {'name':'explicar', 'video':'https://drive.google.com/file/d/1-yPXEYY8Pi3ly_XVFb8ND5ewC4l67ZKB/view?usp=share_link', 'img': 'null'},
+                                        {'name':'entender', 'video':'https://drive.google.com/file/d/1narMLONGXHiNfl283_5OOZE5PlyQ_Li-/view?usp=share_link', 'img': 'null'},
+                                        {'name':'ensenar', 'video':'https://drive.google.com/file/d/1kX5K-iWh1Z8NzagdA4tH456rmhVi2l5d/view?usp=share_link', 'img': 'null'},
+                                        {'name':'escribir', 'video':'https://drive.google.com/file/d/1PV9DVum475bK86GVEKsPouoMUnDbD2Il/view?usp=share_link', 'img': 'null'},
+                                        {'name':'empezar', 'video':'https://drive.google.com/file/d/1JhJYtrFj7L1jQoIBxWPAb8xBRQ40cWlh/view?usp=share_link', 'img': 'null'},
+                                        {'name':'estudiar', 'video':'https://drive.google.com/file/d/1DAXVO261BxSNfGZQE4UcNHMTIo9DAskO/view?usp=share_link', 'img': 'null'},
+                                        {'name':'descansar', 'video':'https://drive.google.com/file/d/1snVQMRrKa4j6yPTIvQK-IM7TZjgzNs7F/view?usp=share_link', 'img': 'null'},
+                                        {'name':'decir', 'video':'https://drive.google.com/file/d/1AO-s5POxhvnd9O0FXipYsEHfJLPXHAem/view?usp=share_link', 'img': 'null'},
+                                        {'name':'dar', 'video':'https://drive.google.com/file/d/14kkgbo5JvtDjR4ksGYm6SAGkJGmJdDtL/view?usp=share_link', 'img': 'null'},
+                                        {'name':'comprar', 'video':'https://drive.google.com/file/d/1o_CK3zJDG4-TAvMpGCHlfXZV_STITe94/view?usp=share_link', 'img': 'null'},
+                                        {'name':'cuidar', 'video':'https://drive.google.com/file/d/1j5WE8NHiagQw_jK0o2n5z2rE3MfT2OAS/view?usp=share_link', 'img': 'null'},
+                                        {'name':'comer', 'video':'https://drive.google.com/file/d/1giPqNXADvifbRWuGun_jHRsTsw58tmk_/view?usp=share_link', 'img': 'null'},
+                                        {'name':'avisar', 'video':'https://drive.google.com/file/d/1Wfe-JYnfS434eIzqGTy2Ngf04A5pJjBL/view?usp=share_link', 'img': 'null'},
+                                        {'name':'creer', 'video':'https://drive.google.com/file/d/1QTmDIZFt3fm8A8238Fuu01UTIPCKA33b/view?usp=share_link', 'img': 'null'},
+                                        {'name':'ayudar', 'video':'https://drive.google.com/file/d/1HT2_GvXEMYJM5Bee7UAx_Ftui671XOHB/view?usp=share_link', 'img': 'null'},
+                                        {'name':'aprender', 'video':'https://drive.google.com/file/d/1ETwID4Y1KVu7t0AAoJcAMY-vLC1dSDjj/view?usp=share_link', 'img': 'null'}
+                                        ]},
+    {'name':'letras', 'level':1, 'words':[
+                                          {'name':'A', 'video':'null', 'img': 'https://drive.google.com/file/d/1QKqGSkKy5a7CYT0hmX9u-kFHB7wP-5_M/view?usp=share_link'},
+                                          {'name':'B', 'video':'null', 'img': 'https://drive.google.com/file/d/1dR3s-hLkT2X5vZ018gF5wIQfIrJR5jEV/view?usp=share_link'},
+                                          {'name':'C', 'video':'null', 'img': 'https://drive.google.com/file/d/1YarH3jcDbTmcsokRw5ZZjGNU2CrXEpmz/view?usp=share_link'},
+                                          {'name':'D', 'video':'null', 'img': 'https://drive.google.com/file/d/1QKqGSkKy5a7CYT0hmX9u-kFHB7wP-5_M/view?usp=share_link'},
+                                          {'name':'E', 'video':'null', 'img': 'https://drive.google.com/file/d/1q0LzcnNB7YiEpd-aCL5lzUZJPcNmPKfA/view?usp=share_link'},
+                                          {'name':'F', 'video':'null', 'img': 'https://drive.google.com/file/d/1Kb5NESMCR8NQMigR8M10r2lsCoPIDRFC/view?usp=share_link'},
+                                          {'name':'G', 'video':'null', 'img': 'https://drive.google.com/file/d/1Kb5NESMCR8NQMigR8M10r2lsCoPIDRFC/view?usp=share_link'},
+                                          {'name':'H', 'video':'null', 'img': 'https://drive.google.com/file/d/1kJHbFQuIq4-uEv7ZFvgIB5NTNbPcIPkB/view?usp=share_link'},
+                                          {'name':'I', 'video':'null', 'img': 'https://drive.google.com/file/d/1kJHbFQuIq4-uEv7ZFvgIB5NTNbPcIPkB/view?usp=share_link'},
+                                          {'name':'J', 'video':'https://drive.google.com/file/d/1CUfKsJ6gsPyJUxkSJHch0-CX_Y3AlZ2w/view?usp=share_link', 'img': 'null'},
+                                          {'name':'L', 'video':'null', 'img': 'https://drive.google.com/file/d/1t6WZb35qND4_bVG8e69wo0UFBjK1GFKP/view?usp=share_link'},
+                                          {'name':'LL', 'video':'https://drive.google.com/file/d/1n486o9HvJbfCDlbpDPvTfz5VocHopLwy/view?usp=share_link', 'img': 'null'},
+                                          {'name':'M', 'video':'null', 'img': 'https://drive.google.com/file/d/13XYgt5aH-d0f-4SAniF6RNnIK5sTRxy6/view?usp=share_link'},
+                                          {'name':'N', 'video':'null', 'img': 'https://drive.google.com/file/d/1LW-Z5_Zu9ndoSewTC_GdSrqbyzTkWcWh/view?usp=share_link'},
+                                          {'name':'Ñ', 'video':'https://drive.google.com/file/d/19imxu_cFgZgK5WNuaAj3SAVWE5iAB_AI/view?usp=share_link', 'img': 'null'},
+                                          {'name':'O', 'video':'null', 'img': 'https://drive.google.com/file/d/19imxu_cFgZgK5WNuaAj3SAVWE5iAB_AI/view?usp=share_link'},
+                                          {'name':'P', 'video':'null', 'img': 'https://drive.google.com/file/d/19imxu_cFgZgK5WNuaAj3SAVWE5iAB_AI/view?usp=share_link'},
+                                          {'name':'Q', 'video':'https://drive.google.com/file/d/1A3k25-Hf5gmRsYCjkD60Yw4ftDT8VNRD/view?usp=share_link', 'img': 'null'},
+                                          {'name':'R', 'video':'null', 'img': 'https://drive.google.com/file/d/1nZpQK8pvLMqtDQpbHrcp2Glb9IX2aIgV/view?usp=share_link'},
+                                          {'name':'RR', 'video':'https://drive.google.com/file/d/1wxFwOHp4fjUXepz16zVrwlAmVN4hFKFd/view?usp=share_link', 'img': 'null'},
+                                          {'name':'S', 'video':'null', 'img': 'https://drive.google.com/file/d/1PgAqrE6n1oxsYpaUlgFUqSYNj2A47Zgz/view?usp=share_link'},
+                                          {'name':'T', 'video':'null', 'img': 'hhttps://drive.google.com/file/d/18DIh-xbUHitO9dPH5DCzim7Hoe8fRKsF/view?usp=share_link'},
+                                          {'name':'U', 'video':'null', 'img': 'https://drive.google.com/file/d/1QZfGLg0PUTDATc21CiAeQglwhoFgAOub/view?usp=share_link'},
+                                          {'name':'V', 'video':'null', 'img': 'https://drive.google.com/file/d/1oOShSo09J44A7ffK8grorZM4WbURwtzm/view?usp=share_link'},
+                                          {'name':'W', 'video':'null', 'img': 'https://drive.google.com/file/d/17BA0BQlvoBuCctby1cgxsO9ajdGrgDRP/view?usp=share_link'},
+                                          {'name':'X', 'video':'https://drive.google.com/file/d/1MsKU9mY01oERDzK8SQTCXcRQHhT9pIIT/view?usp=share_link', 'img': 'null'},
+                                          {'name':'Y', 'video':'null', 'img': 'https://drive.google.com/file/d/1SuTELApPhQK8hEigmW8tBDLHJYncEhu8/view?usp=share_link'},
+                                          {'name':'Z', 'video':'https://drive.google.com/file/d/1rkcHFzQYc--YQZNVLr9SPxvmhWKtrZgq/view?usp=share_link', 'img': 'null'}
+                                          ]}                      
+])
 
 
 '''
