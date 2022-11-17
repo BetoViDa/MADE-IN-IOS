ce4caa0a5dd87040453b5554e7aa4fd092073804
Macias Romero Jorge Humberto <a00573134@itesm.mx>
2022-11-16 20:33:11 -0600

    Algunas actualizaciones

---
+++ Signa/Signa/Views/AdminView.swift
@@ -0,0 +1,20 @@
+//
+//  AdminView.swift
+//  Signa
+//
+//  Created by Macías Romero on 16/11/22.
+//
+
+import SwiftUI
+
+struct AdminView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AdminView_Previews: PreviewProvider {
+    static var previews: some View {
+        AdminView()
+    }
+}
